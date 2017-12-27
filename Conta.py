5006a2130599e7b32f5db61199a86382dc8bdd3d
Andre Chaves <andrechavesg@gmail.com>
2017-12-27 08:32:15 -0200

    capitulo2

---
+++ Conta.py
@@ -0,0 +1,8 @@
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
