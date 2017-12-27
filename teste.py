0e86fafea2459c65b45331ebd42a4cb672a100d4
Andre Chaves <andrechavesg@gmail.com>
2017-12-27 08:28:58 -0200

    capitulo1

---
+++ teste.py
@@ -0,0 +1,12 @@
+def cria_conta(numero, titular, saldo, limite):
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo {}".format(conta["saldo"]))
