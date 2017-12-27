f9e1f7300e5bbb9735d2dee31bd79fe4f313ca8d
Andre Chaves <andrechavesg@gmail.com>
2017-12-27 08:50:33 -0200

    capitulo6

--- Conta.py
+++ Conta.py
@@ -13,8 +13,15 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
@@ -25,7 +32,7 @@ class Conta:
         return self.__saldo
 
     @property
-    def titular(self):
+    def get_titular(self):
         return self.__titular
 
     @property
@@ -35,3 +42,11 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
