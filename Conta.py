1889884b8d2da785db12677f1e02e904a97a7b96
Andre Chaves <andrechavesg@gmail.com>
2017-12-27 08:44:59 -0200

    capitulo5

--- Conta.py
+++ Conta.py
@@ -19,3 +19,19 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
